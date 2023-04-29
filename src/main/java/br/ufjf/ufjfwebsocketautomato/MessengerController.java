0b0f3a9b3cacb50d140637c0e14ac16647afee1f
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-04-29 18:05:44 -0300

    Add endpoint pra retornar json

--- src/main/java/br/ufjf/ufjfwebsocketautomato/MessengerController.java
+++ src/main/java/br/ufjf/ufjfwebsocketautomato/MessengerController.java
@@ -1,8 +1,14 @@
 package br.ufjf.ufjfwebsocketautomato;
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import lombok.val;
 
 @Controller
 public class MessengerController {
@@ -12,6 +18,22 @@ public class MessengerController {
   public Messenger greeting(Greeting greeting) throws Exception {
     Thread.sleep(1000); // simulated delay
     return new Messenger(greeting.greet());
+  }
+
+  @MessageMapping("/sample")
+  @SendTo("/data/sample")
+  public Messenger getExample(Greeting greeting) {
+    var res = "";
+
+    try {
+      val fileName = "automata/" + greeting.getName() + ".json";
+      val is = new ClassPathResource(fileName).getInputStream();
+      res = IOUtils.toString(is, "UTF-8");
+    } catch (IOException e) {
+      res = "Not found";
+    }
+    
+    return new Messenger(res);
   }
 
 }
